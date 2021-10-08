26a6e8eed59069acdbcba619c1ac75a0f9898e41
jethroleeeelorhtej <jethroleeeelorhtej@gmail.com>
2021-10-09 06:31:27 +0800

    Add New Check

--- Domain_DoubleCheck.sh
+++ Domain_DoubleCheck.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 clear
 readonly opApi='Your API'
+readonly pdApi='Your API'
 readonly now=`date +%Y%m%d_%H%M%S`
 readonly yesterday=`date +%Y%m%d --date="-1 day"`
 var="Your directory"
@@ -10,11 +11,18 @@ cat $var/dnsdomain.txt |sort |uniq > $log/dnsdomainlist.txt
 
 echo -e "\E[1;5;33m 撈取全域名中．．． \E[0m "
 curl -d "Your API參數" -s "${opApi}" > $var/PDNS_All_Domain.txt
+echo -ne "\n"
+echo -ne "\n"
+echo -ne "\n"
+echo -e "\E[1;5;33m 撈取anti目標域名中．．． \E[0m "
+curl "${pdApi}" 2&>1 > $var/antiHijackTargetDomain.txt
+echo -ne "\n"
+echo -ne "\n"
+echo -ne "\n"
 
 # 整理DNS error域名資訊
 for domain in `cat $log/dnsdomainlist.txt`; do
     cat $var/PDNS_All_Domain.txt |grep -w "\"domain_name\":\"$domain\"" |jq -c [.domain_name,.site_group,.status_id,.create_time,.note_gm,.note_id] |sed -e 's/^\[//g;s/\]$//g'|sed 's/^\"//g;s/\"$//g'|awk -F \"\,\" '{print $1, $2, $3, $4, $5, $6}' >> $log/dnsdomainall_${now}.txt
-    echo -ne "$domain \033[33m資料撈取完畢\033[0m\n"
     echo -ne "$domain \033[33m資料撈取完畢\033[0m\n"
 done
 
@@ -33,6 +41,10 @@ for dnsname in `cat $log/dnsdomainlist.txt`; do
     timecheck=`date +%s -d "${nowtime}"`
     timecheck2=`date +%s -d "${dnscreatetime}"`
     timecompare=`echo "(${timecheck} - ${timecheck2}) / 86400" |bc`
+    dnsantiHijackTarget=`cat $var/antiHijackTargetDomain.txt |grep -w $dnsname`
+    dnsantiHijack=`cat $log/dnsgrepdomain_${now}.txt |grep -w $dnsname |grep -w "${yesterday}檢查網址DNS劫持\[全域\]" |awk '{print $7}' |grep -w "抗劫持"`
+    dnsantiBlockTarget=`cat $log/dnsgrepdomain_${now}.txt |grep -w $dnsname |awk '{print $7}' |grep -w "抗封鎖目標網址"`
+    dnsantiBlock=`cat $log/dnsgrepdomain_${now}.txt |grep -w $dnsname |grep -w "${yesterday}檢查網址DNS劫持\[全域\]" |awk '{print $7}' |grep -w "抗封鎖"`
     if [ "$dnsname" == "$listcheck" ] && [ ${timecompare} -gt "14" ]; then
         echo -ne "$dnssitegroup $dnsname  \033[32mPDNS有備註且網址不是NEW\033[0m $dnsstatusid \033[32m$dnsnotegm\033[0m $dnsnoteid \n"
         echo -ne "$dnssitegroup $dnsname  \n" >> $log/listok_${now}.txt
@@ -42,6 +54,13 @@ for dnsname in `cat $log/dnsdomainlist.txt`; do
     else
         echo -ne "$dnssitegroup $dnsname \033[31mPDNS沒有備註\033[0m\n"
         echo -ne "$dnssitegroup $dnsname  \n" >> $log/listerror_${now}.txt
+    fi
+
+    if [ -n "${dnsantiHijackTarget}" ] || [ -n "${dnsantiHijack}" ] || [ -n "${dnsantiBlockTarget}" ] || [ -n "${dnsantiBlock}" ]; then
+        echo -ne "$dnssitegroup $dnsname \033[31mDNS error，要撤下相關功能\033[0m\n"
+        echo -ne "$dnssitegroup $dnsname \033[31mDNS error，要撤下相關功能\033[0m\n" >> $log/rollback_${now}.txt
+    else
+        echo -ne "$dnssitegroup $dnsname \n" >> $log/dontrollback_${now}.txt
     fi
 done
 
@@ -72,12 +91,22 @@ for MBname in `cat $var/MobileBlockdomainlist.txt`; do
     MBstatusid=`cat $log/MBgrepdomain_${now}.txt |grep -w $MBname |awk '{print $3}'`
     MBnotegm=`cat $log/MBgrepdomain_${now}.txt |grep -w $MBname |awk '{print $4}'`
     MBnoteid=`cat $log/MBgrepdomain_${now}.txt |grep -w $MBname |awk '{print $5}'`
+    MBantiHijackTarget=`cat $var/antiHijackTargetDomain.txt |grep -w $MBname`
+    MBantiHijack=`cat $log/MBgrepdomain_${now}.txt |grep -w $MBname |awk '{print $5}' |grep -w "抗劫持"`
+    MBantiBlockTarget=`cat $log/MBgrepdomain_${now}.txt |grep -w $MBname |awk '{print $5}' |grep -w "抗封鎖目標網址"`
     if [ "${MBstatusid}" == "D-移動封鎖自管" ] || [ "${MBstatusid}" == "D-移動封鎖" ] && [ "${MBname}" == "${MBlistcheck}" ]; then
         echo -ne "$MBsitegroup $MBname  \033[32mPDNS狀態已修改\033[0m $MBstatusid \033[32mPDNS備註已加\033[0m $MBnotegm $MBnoteid \n"
         echo -ne "$MBsitegroup $MBname  \n" >> $log/MBlistok_${now}.txt
     else
         echo -ne "$MBsitegroup $MBname \033[31mPDNS沒有改狀態或備註\033[0m\n"
         echo -ne "$MBsitegroup $MBname  \n" >> $log/MBlisterror_${now}.txt
+    fi
+
+    if [ -n "${MBantiHijackTarget}" ] || [ -n "${MBantiHijack}" ] || [ -n "${MBantiBlockTarget}" ]; then
+        echo -ne "$MBsitegroup $MBname \033[31mMB block，要撤下相關功能\033[0m\n"
+        echo -ne "$MBsitegroup $MBname \033[31mMB block，要撤下相關功能\033[0m\n" >> $log/rollback_${now}.txt
+    else
+        echo -ne "$MBsitegroup $MBname \n" >> $log/dontrollback_${now}.txt
     fi
 done
 
@@ -108,12 +137,23 @@ for NSname in `cat $var/NSerrordomainlist.txt`; do
     NSstatusid=`cat $log/NSgrepdomain_${now}.txt |grep -w $NSname |awk '{print $3}'`
     NSnotegm=`cat $log/NSgrepdomain_${now}.txt |grep -w $NSname |awk '{print $4}'`
     NSnoteid=`cat $log/NSgrepdomain_${now}.txt |grep -w $NSname |awk '{print $5}'`
+    NSantiHijackTarget=`cat $var/antiHijackTargetDomain.txt |grep -w $NSname`
+    NSantiHijack=`cat $log/NSgrepdomain_${now}.txt |grep -w $NSname |awk '{print $5}' |grep -w "抗劫持"`
+    NSantiBlockTarget=`cat $log/NSgrepdomain_${now}.txt |grep -w $NSname |awk '{print $5}' |grep -w "抗封鎖目標網址"`
+    NSantiBlock=`cat $log/NSgrepdomain_${now}.txt |grep -w $NSname |awk '{print $5}' |grep -w "抗封鎖"`
     if [ "${NSstatusid}" == "N-網址上層/解析異常" ] && [ "${NSname}" == "${NSlistcheck}" ]; then
         echo -ne "$NSsitegroup $NSname  \033[32mPDNS狀態已修改\033[0m $NSstatusid \033[32mPDNS備註已加\033[0m $NSnotegm $NSnoteid \n"
         echo -ne "$NSsitegroup $NSname  \n" >> $log/NSlistok_${now}.txt
     else
         echo -ne "$NSsitegroup $NSname \033[31mPDNS沒有改狀態或備註\033[0m\n"
         echo -ne "$NSsitegroup $NSname  \n" >> $log/NSlisterror_${now}.txt
+    fi
+
+    if [ -n "${NSantiHijackTarget}" ] || [ -n "${NSantiHijack}" ] || [ -n "${NSantiBlockTarget}" ] || [ -n "${NSantiBlock}" ]; then
+        echo -ne "$NSsitegroup $NSname \033[31mNS error，要撤下相關功能\033[0m\n"
+        echo -ne "$NSsitegroup $NSname \033[31mNS error，要撤下相關功能\033[0m\n" >> $log/rollback_${now}.txt
+    else
+        echo -ne "$NSsitegroup $NSname \n" >> $log/dontrollback_${now}.txt
     fi
 done
 
@@ -144,12 +184,22 @@ for ALLname in `cat $var/AllBlockdomainlist.txt`; do
     ALLstatusid=`cat $log/ALLgrepdomain_${now}.txt |grep -w $ALLname |awk '{print $3}'`
     ALLnotegm=`cat $log/ALLgrepdomain_${now}.txt |grep -w $ALLname |awk '{print $4}'`
     ALLnoteid=`cat $log/ALLgrepdomain_${now}.txt |grep -w $ALLname |awk '{print $5}'`
+    ALLantiHijackTarget=`cat $var/antiHijackTargetDomain.txt |grep -w $ALLname`
+    ALLantiHijack=`cat $log/ALLgrepdomain_${now}.txt |grep -w $ALLname |awk '{print $5}' |grep -w "抗劫持"`
+    ALLantiBlockTarget=`cat $log/ALLgrepdomain_${now}.txt |grep -w $ALLname |awk '{print $5}' |grep -w "抗封鎖目標網址"`
     if [ "${ALLstatusid}" == "D-封鎖自管" ] || [ "${ALLstatusid}" == "D-封鎖" ] && [ "${ALLname}" == "${ALLlistcheck}" ]; then
         echo -ne "$ALLsitegroup $ALLname  \033[32mPDNS狀態已修改\033[0m $ALLstatusid \033[32mPDNS備註已加\033[0m $ALLnotegm $ALLnoteid \n"
         echo -ne "$ALLsitegroup $ALLname  \n" >> $log/ALLlistok_${now}.txt
     else
         echo -ne "$ALLsitegroup $ALLname \033[31mPDNS沒有改狀態或備註\033[0m\n"
         echo -ne "$ALLsitegroup $ALLname  \n" >> $log/ALLlisterror_${now}.txt
+    fi
+
+    if [ -n "${ALLantiHijackTarget}" ] || [ -n "${ALLantiHijack}" ] || [ -n "${ALLantiBlockTarget}" ]; then
+        echo -ne "$ALLsitegroup $ALLname \033[31m網址為全域封鎖，要撤下抗劫持抗封鎖相關功能\033[0m\n"
+        echo -ne "$ALLsitegroup $ALLname \033[31m網址為全域封鎖，要撤下抗劫持抗封鎖相關功能\033[0m\n" >> $log/rollback_${now}.txt
+    else
+        echo -ne "$ALLsitegroup $ALLname \n" >> $log/dontrollback_${now}.txt
     fi
 done
 
@@ -191,7 +241,15 @@ echo -ne "\n"
 echo -ne "\n"
 echo -ne "\n"
 echo -ne "\n"
+echo -ne "\033[33m↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓\033[0m  需撤下相關功能 之網址  若無檔案代表皆正常  \033[33m↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓\033[0m\n"
+cat $log/rollback_${now}.txt
+echo -ne "\033[33m↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑\033[0m  需撤下相關功能 之網址  若無檔案代表皆正常  \033[33m↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑\033[0m\n"
+echo -ne "\n"
+echo -ne "\n"
+echo -ne "\n"
+echo -ne "\n"
 echo -ne "\033[33m==========\033[0m DNS error網址PDNS沒備註或是網址為NEW清單請查看 $log/listerror_${now}.txt  若無檔案代表皆正常  \033[33m==========\033[0m\n"
 echo -ne "\033[33m==========\033[0m MB block網址PDNS沒改狀態或備註清單請查看 $log/MBlisterror_${now}.txt  若無檔案代表皆正常  \033[33m==========\033[0m\n"
 echo -ne "\033[33m==========\033[0m NS error網址PDNS沒改狀態或備註清單請查看 $log/NSlisterror_${now}.txt  若無檔案代表皆正常  \033[33m==========\033[0m\n"
 echo -ne "\033[33m==========\033[0m ALL block網址PDNS沒改狀態或備註清單請查看 $log/ALLlisterror_${now}.txt  若無檔案代表皆正常  \033[33m==========\033[0m\n"
+echo -ne "\033[33m==========\033[0m 需撤相關功能清單請查看 $log/rollback_${now}.txt  若無檔案代表皆正常  \033[33m==========\033[0m\n"
